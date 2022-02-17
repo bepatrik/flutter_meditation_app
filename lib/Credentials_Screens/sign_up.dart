eee727078f3d065e0fad50384e6ade8f7d80b761
Ali ijaz <Se.Aliijaz@gmail.com>
2022-02-17 22:59:47 +0500

    Blur Image

--- lib/Credentials_Screens/sign_up.dart
+++ lib/Credentials_Screens/sign_up.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_meditation_app/Constants/constants.dart';
@@ -21,103 +23,109 @@ class SignUpScreen extends StatelessWidget {
             fit: BoxFit.fill,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Form(
-              child: Container(
-                height: s.height * 0.60,
-                width: s.width,
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      'SIGNUP FORM',
-                      style: GoogleFonts.lateef(
-                        textStyle: TextStyle(
-                          fontSize: 30.5,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                          prefixIcon: Icon(Icons.person),
-                          hintText: 'Enter Name',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                          prefixIcon: Icon(Icons.email),
-                          hintText: 'Enter E-mail',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                          prefixIcon: Icon(Icons.lock),
-                          hintText: 'Enter Password',
-                          suffixIcon: Icon(Icons.visibility),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: MaterialButton(
-                        shape: StadiumBorder(),
-                        height: 55,
-                        minWidth: s.width,
-                        color: Colors.blue,
-                        onPressed: () {},
-                        child: Text(
-                          'SIGNUP',
-                          style: TextStyle(
-                            color: Colors.white,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 10,
+            sigmaY: 10,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Form(
+                child: Container(
+                  height: s.height * 0.60,
+                  width: s.width,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'SIGNUP FORM',
+                        style: GoogleFonts.lateef(
+                          textStyle: TextStyle(
+                            fontSize: 30.5,
                             fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
                           ),
                         ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text("Already have account?"),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text('LOGIN NOW'),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            prefixIcon: Icon(Icons.person),
+                            hintText: 'Enter Name',
+                          ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            prefixIcon: Icon(Icons.email),
+                            hintText: 'Enter E-mail',
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            prefixIcon: Icon(Icons.lock),
+                            hintText: 'Enter Password',
+                            suffixIcon: Icon(Icons.visibility),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: MaterialButton(
+                          shape: StadiumBorder(),
+                          height: 55,
+                          minWidth: s.width,
+                          color: Colors.blue,
+                          onPressed: () {},
+                          child: Text(
+                            'SIGNUP',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Already have account?"),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('LOGIN NOW'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
