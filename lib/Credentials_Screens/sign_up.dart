f6f22fa05926b4d79d60939077924f1de9b4f8e0
Ali ijaz <Se.Aliijaz@gmail.com>
2022-02-18 00:28:14 +0500

    login and signup functionality

--- lib/Credentials_Screens/sign_up.dart
+++ lib/Credentials_Screens/sign_up.dart
@@ -1,13 +1,58 @@
 import 'dart:ui';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_meditation_app/Constants/constants.dart';
+import 'package:flutter_meditation_app/Credentials_Screens/log_in.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SignUpScreen extends StatelessWidget {
   ///final id
   static final id = '/SignUpScreen';
+
+  ///Controllers
+  TextEditingController nameC = TextEditingController();
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passC = TextEditingController();
+
+  ///global key
+  var globalKey = GlobalKey<FormState>();
+
+  ///auth
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  saveFormUser(BuildContext context) async {
+    try {
+      if (globalKey.currentState.validate()) {
+        print(emailC.text);
+        await auth.createUserWithEmailAndPassword(
+          email: emailC.text.trim(),
+          password: passC.text.trim(),
+        );
+        var user = auth.currentUser.uid;
+        if (user != null) {
+          await userRef.child(auth.currentUser.uid).set(
+            {
+              "Name": nameC.text,
+              "Email": emailC.text,
+              "Password": passC.text,
+            },
+          ).catchError(
+            (onError) {
+              toastmsg(
+                onError.toString(),
+              );
+            },
+          );
+          await Navigator.pushNamed(context, LogInScreen.id);
+        }
+      }
+    } catch (e) {
+      await toastmsg(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     ///size
@@ -31,6 +76,7 @@ class SignUpScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Form(
+                key: globalKey,
                 child: Container(
                   height: s.height * 0.60,
                   width: s.width,
@@ -54,6 +100,18 @@ class SignUpScreen extends StatelessWidget {
                       Padding(
                         padding: EdgeInsets.all(8.0),
                         child: TextFormField(
+                          controller: nameC,
+                          validator: (v) {
+                            try {
+                              if (nameC.text.length < 3) {
+                                return 'Name should be greater than 3 words';
+                              } else {
+                                return null;
+                              }
+                            } catch (e) {
+                              return toastmsg(e.toString());
+                            }
+                          },
                           decoration: InputDecoration(
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(50),
@@ -66,6 +124,21 @@ class SignUpScreen extends StatelessWidget {
                       Padding(
                         padding: EdgeInsets.all(8.0),
                         child: TextFormField(
+                          controller: emailC,
+                          validator: (v) {
+                            try {
+                              if (!emailC.text.contains('@gmail.com')) {
+                                return 'Enter valid email';
+                              } else if (emailC.text
+                                  .contains('@gmail.com'.toUpperCase())) {
+                                return 'Please Enter valid email';
+                              } else {
+                                return null;
+                              }
+                            } catch (e) {
+                              return toastmsg(e.toString());
+                            }
+                          },
                           decoration: InputDecoration(
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(50),
@@ -78,6 +151,18 @@ class SignUpScreen extends StatelessWidget {
                       Padding(
                         padding: EdgeInsets.all(8.0),
                         child: TextFormField(
+                          controller: passC,
+                          validator: (v) {
+                            try {
+                              if (passC.text.length < 7) {
+                                return 'Password should be greater than 3 words';
+                              } else {
+                                return null;
+                              }
+                            } catch (e) {
+                              return toastmsg(e.toString());
+                            }
+                          },
                           obscureText: true,
                           decoration: InputDecoration(
                             border: OutlineInputBorder(
@@ -96,7 +181,9 @@ class SignUpScreen extends StatelessWidget {
                           height: 55,
                           minWidth: s.width,
                           color: Colors.blue,
-                          onPressed: () {},
+                          onPressed: () {
+                            saveFormUser(context);
+                          },
                           child: Text(
                             'SIGNUP',
                             style: TextStyle(
