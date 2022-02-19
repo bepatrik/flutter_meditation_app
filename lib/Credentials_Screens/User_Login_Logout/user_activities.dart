bf748eaefe669fc480a28ce1798c232d6c4fe40b
Ali ijaz <Se.Aliijaz@gmail.com>
2022-02-19 22:56:33 +0500

    Comments

--- lib/Credentials_Screens/User_Login_Logout/user_activities.dart
+++ lib/Credentials_Screens/User_Login_Logout/user_activities.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_meditation_app/Constants/constants.dart';
 import 'package:flutter_meditation_app/Credentials_Screens/log_in.dart';
 import 'package:flutter_meditation_app/Home_Screen/home_screen.dart';
 import 'package:flutter_meditation_app/Widgets/loading_effect.dart';
@@ -27,12 +28,24 @@ class UserLogInActivity extends StatelessWidget {
               ///Notifies about changes to the user's sign-in state (such as sign-in or sign-out).
               initialData: auth.currentUser,
               builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+                ///Connected to an asynchronous computation and awaiting interaction.
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return Center(child: LoadingEffect());
                 }
+                /*
+                Returns whether this snapshot contains a non-null [error] value.
+                This is always true if the asynchronous computation's last result was failure.*/
+                if (snapshot.hasError) {
+                  return toastmsg(snapshot.error.toString());
+                }
+                /* The latest data received by the asynchronous computation.
+                If this is non-null, [hasData] will be true.
+                If [error] is not null, this will be null. See [hasError].
+                If the asynchronous computation has never returned a value,
+                this may be set to an initial data value specified by the relevant widget.
+                See [FutureBuilder.initialData] and [StreamBuilder.initialData].*/
                 if (snapshot.data != null) {
-                  // ignore: unused_local_variable
-                  User user = snapshot.data;
+                  // User user = snapshot.data;
                   return HomeScreen();
                 } else {
                   return LogInScreen();
